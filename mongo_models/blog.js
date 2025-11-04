9a5cba336d829f2f7351d22e4b5f3044176f71bc
Anindya Dutta <dutta.ani007@gmail.com>
2025-11-04 08:50:37 +0530

    Update blog.js

--- mongo_models/blog.js
+++ mongo_models/blog.js
@@ -5,6 +5,8 @@ import mongoose from "mongoose";
 const blogSchema = new mongoose.Schema(
     {
         title: {type: String, required: true},
+        image_url: {type: String, required: true},
+        tags: {type: [String], required: true},
         category: {type: String, required: true},
         content: {type: String, required: true}
     },
